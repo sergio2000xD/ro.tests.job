3b0e27956ab498218ce81f75bec8d0452f1726fa
Rodrigo <correo.rodrigo@gmail.com>
2014-05-14 15:52:23 -0500

    css examples

---
+++ ro.tests.job/Controllers/CssController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ro.tests.job.Controllers
+{
+    public class CssController : Controller
+    {
+        //
+        // GET: /Css/
+
+        public ActionResult Position()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ro.tests.job/Controllers/JsUnitTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ro.tests.job.Controllers
+{
+    public class JsUnitTestController : Controller
+    {
+        //
+        // GET: /JsUnitTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
