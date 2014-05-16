577cdfa6c8298fec93cb729be3dfb5b4bfed9ad8
Rodrigo <correo.rodrigo@gmail.com>
2014-05-16 10:08:50 -0500

    gant

---
+++ ro.tests.job/Controllers/GantController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ro.tests.job.Controllers
+{
+    public class GantController : Controller
+    {
+        //
+        // GET: /Gant/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ro.tests.job/Scripts/Gant/gant.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    this.ro = this.ro || {};
+    var ns = this.ro;
+
+    ns.GantParams = {
+        skipDays: ["saturday", "sunday"],
+        table: undefined,
+        start: undefined,
+        end: undefined
+    };
+
+    ns.GantException = (function (message) { 
+        this.message = message;
+        this.name = "GantException";
+    })();
+
+    ns.Gant = (function () {
+        function Gant(table, start, end, gantParams) {
+            var _that = this;
+            var _params = gantParams || ns.GantParams();
+
+            _that.setTable = function (table) {
+                if (!table) {
+                    throw new ns.GantException("undefined table");
+                }
+                _params.table = table;
+            }
+
+            _that.setEndDate = function (end) {
+                if (!end || !end instanceof Date) {
+                    throw new ns.GantException("invalid end date");
+                }
+                _params.end = end;
+            }
+
+            _that.setStartDate = function (start) {
+                if (!start || !start instanceof Date) {
+                    throw new ns.GantException("invalid start date");
+                }
+                _params.start = start;
+            }
+
+            _that.getParams = function () {
+                return _params;
+            };
+
+            _that.setStartDate(start);
+            _that.setEndDate(end);                        
+        }
+
+        Gant.prototype.init = function () {
+            
+        }
+
+        return Gant;
+    })();
+
+}());
