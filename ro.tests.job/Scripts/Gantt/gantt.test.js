f000cc401ad591e02c0db104379aca10d485f9d3
Rodrigo <correo.rodrigo@gmail.com>
2014-05-16 15:23:28 -0500

    adding tests

--- ro.tests.job/Scripts/Gantt/gantt.test.js
+++ ro.tests.job/Scripts/Gantt/gantt.test.js
@@ -33,3 +33,27 @@ test("start and end dates", 1, function () {
     equal(params.table, table, "table is ok");
 });
 
+
+test("convert milliseconds to days", 1, function () {
+    expect(3);
+    
+    var start = new Date(2014, 1, 1);
+    var end = new Date(2015, 1, 1);
+
+    var table = $("table:first");
+
+    var gantt = new ro.GanttChart(table,
+        new Date(start.getFullYear(), start.getMonth(), start.getDay()),
+        new Date(end.getFullYear(), end.getMonth(), end.getDay()));
+
+    var span1 = new Date(start.getFullYear(), start.getMonth(), start.getDay()).setDate(start.getDay() + 1) - gantt.getParams().start;
+    var span32 = new Date(start.getFullYear(), start.getMonth(), start.getDay()).setDate(start.getDay() + 32) - gantt.getParams().start;
+    var span365 = new Date(start.getFullYear(), start.getMonth(), start.getDay()).setDate(start.getDay() + 365) - gantt.getParams().start;
+
+    
+
+    equal(gantt.millisecondsToDays(span1), 1, "one day");
+    equal(gantt.millisecondsToDays(span32), 32, "one month");
+    equal(gantt.millisecondsToDays(span365), 365, "one year");
+});
+
