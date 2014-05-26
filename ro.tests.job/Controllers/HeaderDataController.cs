018440506842cab72a7669e80c171565dfeb1a8f
Rodrigo <correo.rodrigo@gmail.com>
2014-05-26 16:15:46 -0500

    added class for js testing

---
+++ ro.tests.job/Controllers/HeaderDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ro.tests.job.Controllers
+{
+    public class HeaderDataController : Controller
+    {
+        //
+        // GET: /HeaderData/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
